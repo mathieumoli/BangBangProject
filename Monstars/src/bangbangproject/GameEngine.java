60197f61850f8750871ccfa904da31c17f028c56
unknown <user@BXXX.polytech.unice.fr>
2015-03-11 07:50:41 +0100

    added files for game

---
+++ Monstars/src/bangbangproject/GameEngine.java
@@ -0,0 +1,19 @@
+package bangbangproject;
+
+public class GameEngine {
+	public GameEngine(int roundNumber, boolean mode){
+		
+	}
+	/**
+	 * The games wait 3 sec minimum for the countdown + a random time
+	 */
+	public void launchRound(){
+		
+	}
+	public boolean playerShot(int player){
+		return true;
+	}
+	public int getPlayerScore(int player){
+		return 0;
+	}
+}
---
+++ Monstars/src/bangbangproject/GameWindow.java
@@ -0,0 +1,47 @@
+package bangbangproject;
+
+import devintAPI.FenetreAbstraite;
+
+public class GameWindow extends FenetreAbstraite {
+
+	public GameWindow(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	
+	}
+	public GameWindow(int roundNumbers, boolean gameType){
+		super("Game ON");
+		
+	}
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	protected String wavAide() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void changeColor() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void changeSize() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	protected String wavAccueil() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	protected String wavRegleJeu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
